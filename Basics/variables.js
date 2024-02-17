4337afd17481ec1b8c9ebbe218dc87b821a57888
Suraj Chaudhary <surajchaudhary6613@gmail.com>
2024-02-17 14:36:07 +0530

    JavaScript Variables

---
+++ Basics/variables.js
@@ -0,0 +1,23 @@
+const userId = 6613;
+let userName = "Suraj Chaudhary";
+let userEmail = "suraj@gmail.com";
+var userPassword = "Suraj@1234";
+userCity = "Lucknow";
+let userState;
+
+console.log("Before any Changes");
+console.log(userId);
+console.table([userId, userName, userEmail, userCity, userPassword, userState]);
+
+//Trying to change values of variables
+
+// userId = 223    // Can't change a constant value
+userEmail = "newEmail@gmail.com";
+userPassword = "newPassword@1234";
+userCity = "Basti";
+userName = "Suraj Patel"
+userState = "Uttar Pradesh";
+
+console.log("After Changes");
+console.log(userId);
+console.table([userId, userName, userEmail, userCity, userPassword, userState]);
