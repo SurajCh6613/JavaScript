bda83404ab098ff15c5893d5db90bb2d48efc154
Suraj Chaudhary <surajchaudhary6613@gmail.com>
2024-02-17 16:19:26 +0530

    Comparisons in JavaScript

---
+++ Basics/comparison.js
@@ -0,0 +1,30 @@
+// Basic and good comparisons
+
+// console.log(2 > 1);      //true
+// console.log(2 < 1);      //false
+// console.log(2 == 1);     //false
+// console.log(2 >= 1);     //true
+// console.log(2 <= 1);     //false
+// console.log(2 != 1);     //true
+
+
+// console.log("2" > 1);       //true
+// console.log("2" == 2);       //true
+
+// Avoid to do comparisons like below
+
+// console.log(null > 0);      //false
+// console.log(null < 0);      //false
+// console.log(null >= 0);     //true
+// console.log(null <= 0);     //true
+
+//  console.log(undefined > 0);      //false
+// console.log(undefined < 0);      //false
+// console.log(undefined >= 0);     //false
+// console.log(undefined <= 0);     //false
+
+// Strict Comparison means comparison along with type checking
+// done using ===
+
+// console.log("2" === 2);  //false
+// console.log("2" === "2");  //true
