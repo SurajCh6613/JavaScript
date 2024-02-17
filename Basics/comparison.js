9ed2c897dce73d21dc8bfd705d4996d4f484ec31
Suraj Chaudhary <surajchaudhary6613@gmail.com>
2024-02-17 16:20:40 +0530

    Comment added

--- Basics/comparison.js
+++ Basics/comparison.js
@@ -1,4 +1,4 @@
-// Basic and good comparisons
+// //Basic and good comparisons
 
 // console.log(2 > 1);      //true
 // console.log(2 < 1);      //false
@@ -11,7 +11,7 @@
 // console.log("2" > 1);       //true
 // console.log("2" == 2);       //true
 
-// Avoid to do comparisons like below
+// //Avoid to do comparisons like below
 
 // console.log(null > 0);      //false
 // console.log(null < 0);      //false
@@ -23,8 +23,8 @@
 // console.log(undefined >= 0);     //false
 // console.log(undefined <= 0);     //false
 
-// Strict Comparison means comparison along with type checking
-// done using ===
+// //Strict Comparison means comparison along with type checking
+// //done using ===
 
 // console.log("2" === 2);  //false
 // console.log("2" === "2");  //true
