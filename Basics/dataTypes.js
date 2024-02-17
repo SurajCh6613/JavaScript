d5b94dde8caa9762a104336c27cbd3f2d165a9d7
Suraj Chaudhary <surajchaudhary6613@gmail.com>
2024-02-17 14:59:25 +0530

    DataTypes in JS

---
+++ Basics/dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //Treat all JS code as newer version
+
+
+let name = "Suraj"
+let age = 20
+let isEligible = true
+let state;
+let city = null;
+
+
+//DataTypes in JavaScript
+
+// Number =>  range upto 2^53
+// BigInt
+// String => ""
+// Boolean => true/false
+// undefined 
+// null => standalone value
+// Symbol =>unique
+
+// oblect
+
+console.log(typeof state); //undefined
+console.log(typeof city); //return Object
