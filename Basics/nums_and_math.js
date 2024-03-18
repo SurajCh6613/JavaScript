5f77847143ede1970aa18317b1e231dcc65ea973
Suraj Chaudhary <surajchaudhary6613@gmail.com>
2024-03-18 19:15:36 +0530

    Maths and Number

---
+++ Basics/nums_and_math.js
@@ -0,0 +1,34 @@
+const score = 500
+// console.log(score);
+
+const newScore = new Number(400)
+
+// console.log(newScore);
+// console.log(newScore.toString());
+// console.log(newScore.toString().length);
+// console.log(newScore.toFixed(2));
+
+const balance = 123.8966
+// console.log(balance.toPrecision(3));
+
+const currency = 1000000
+// console.log(currency.toLocaleString());
+// console.log(currency.toLocaleString('en-IN'));
+
+// ++++++++++++++ Math ++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.4));
+// console.log(Math.round(4.9));
+// console.log(Math.max(4,5,3,2,7,8,10));
+// console.log(Math.min(4,5,3,2,7,8,10));
+
+// console.log(Math.random());
+// console.log((Math.random()*10)+1);
+// console.log(Math.floor((Math.random()*10)+1));
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
