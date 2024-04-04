9f1e765fb7237ff4de61bb325c80e8e199934aed
Suraj Chaudhary <surajchaudhary6613@gmail.com>
2024-04-04 21:10:48 +0530

    For Loop Techniques

---
+++ Loops/for_loop.js
@@ -0,0 +1,27 @@
+// For Loop
+
+for(let i=0; i<5; i++){
+    console.log(i);
+ }
+ 
+ 
+ // For-in Loop
+ 
+ let obj={
+     Suraj:98,
+     Atul:99,
+     Vikrant:100,
+     "Ram Anuj Pathak":101,
+     Satyendra :102
+ }
+ for (const a in obj) {
+    console.log(a);
+    
+ }
+ console.log(obj)
+ 
+ // For-of Loop
+ let b="Suraj"
+ for (const a of b) {
+     console.log(a)
+ }
