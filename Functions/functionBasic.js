4f7e0ee743702920ffed07c4bdb980819c420d57
Suraj Chaudhary <surajchaudhary6613@gmail.com>
2024-05-01 20:48:19 +0530

    Function

---
+++ Functions/functionBasic.js
@@ -0,0 +1,21 @@
+function myName(){
+    console.log('S');
+    console.log('u');
+    console.log('r');
+    console.log('a');
+    console.log('j');
+}
+
+// myName();
+
+function addTwoNumbers(num1,num2){
+    console.log(num1+num2);
+}
+
+// addTwoNumbers(3,4)          //7
+// addTwoNumbers(3,'4')        //34
+// addTwoNumbers(3,'a')        //3a
+// addTwoNumbers(3,null)       //3
+
+// const result = addTwoNumbers(3,4)       //print 7 by function body
+// console.log("Result: ",result);         //print undefined due to no return from function body
