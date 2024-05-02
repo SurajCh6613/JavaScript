395182c55d9e59032cb2fdeae56852f1f50c5f0c
Suraj Chaudhary <surajchaudhary6613@gmail.com>
2024-05-02 19:15:14 +0530

    Return function and parameter

--- Functions/functionBasic.js
+++ Functions/functionBasic.js
@@ -7,10 +7,10 @@ function myName(){
 }
 
 // myName();
-
-function addTwoNumbers(num1,num2){
-    console.log(num1+num2);
-}
+// Fuction without return
+// function addTwoNumbers(num1,num2){
+//     console.log(num1+num2);
+// }
 
 // addTwoNumbers(3,4)          //7
 // addTwoNumbers(3,'4')        //34
@@ -19,3 +19,24 @@ function addTwoNumbers(num1,num2){
 
 // const result = addTwoNumbers(3,4)       //print 7 by function body
 // console.log("Result: ",result);         //print undefined due to no return from function body
+
+// Fuction with return
+function addTwoNumbers(num1,num2){
+    // let result = num1 + num2
+    // return result
+    return num1+num2
+}
+
+// const result = addTwoNumbers(3,4)       //return 7 by function body to result variable
+// console.log("Result: ",result);         //print 7
+
+function loginUserMessage(username = 'suraj'){
+    if(username === undefined){         //or if(!undefined){    }
+        console.log("Please enter user name:");
+        return
+    }
+    return `${username} just logged in`
+}
+
+console.log(loginUserMessage());        //suraj just logged in
+console.log(loginUserMessage("Vikrant"));        //Vikrant just logged in by override
