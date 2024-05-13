dbaa49936af7feb132931041c21cce1753625874
Suraj Chaudhary <surajchaudhary6613@gmail.com>
2024-05-13 15:17:41 +0530

    Keyboard Check program

---
+++ DOM-Projects/Keyboard-check/script.js
@@ -0,0 +1,20 @@
+const insert = document.querySelector("#insert");
+
+window.addEventListener("keydown", (e) => {
+  insert.innerHTML = `
+        <div class="color"></div>
+        <table>
+        <tr>
+            <th>Key</th>
+            <th>Keycode</th>
+            <th>Code</th>
+        </tr>
+        <tr>
+            <td>${e.key === ' '? 'Space' : e.key}</td>
+            <td>${e.keyCode}</td>
+            <td>${e.code}</td>
+        </tr>
+      
+        </table>
+    `;
+});
