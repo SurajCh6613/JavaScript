d2909baa01edaaf10b9bd97509d1c3d057d005f0
Suraj Chaudhary <surajchaudhary6613@gmail.com>
2024-05-23 12:54:09 +0530

    Get Set over function properties

---
+++ Class-and-Object/functionProperties_get_set.js
@@ -0,0 +1,26 @@
+function User(email, password){
+    this._email = email
+    this._password = password
+
+    Object.defineProperty(this, 'email', {
+        get: function(){
+            return this._email.toUpperCase()
+        },
+        set: function(value){
+            this._email = value
+        }
+    })
+
+    Object.defineProperty(this, 'password', {
+        get: function(){
+            return this._password.toUpperCase()
+        },
+        set: function(value){
+            this._password = value
+        }
+    })
+
+}
+
+const suraj = new User('suraj@gmail.com', '123')
+console.log(suraj.email);
